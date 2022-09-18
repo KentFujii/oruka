06805aa88f14c143c1d005a50642060f59d92e0b
KentFujii <kent.where.the.light.is@gmail.com>
2022-09-19 01:21:40 +0900

    first_commit

---
+++ lib/oruka.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "oruka/version"
+
+module Oruka
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/oruka_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Oruka do
+  it "has a version number" do
+    expect(Oruka::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
